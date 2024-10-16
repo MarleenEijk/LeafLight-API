4f66a9b75c7549a7e9b50e3aaccd9c4218a97811
Marleen <marleeneijkemans@gmail.com>
2024-10-16 09:42:33 +0200

    Deleting classes which have no use yet

--- LeafLight-API/Models/Issue.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LeafLight_API.Models
-{
-    public class Issue
-    {
-        public int Id { get; set; }
-        public string Cause { get; set; }
-        public string Solution { get; set; }
-    }
-}
